b0d279d8600af15bd91b8a50750b54deed1bf85b
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-06-16 20:51:42 +0530

    if If_else

--- If_else.cpp
+++ If_else.cpp
@@ -3,11 +3,15 @@ using namespace std;
 int main()
 {
     int age;
-    cout << "Enter your age = " ;
+    cout << "Enter your age = ";
     cin >> age;
     if (age >= 18)
     {
         cout << "you are eligible to vote" << endl;
+    }
+    if (age >= 35) // we can use multiple if statement.
+    {
+        cout << "Contest for election";
     }
     else
     {
