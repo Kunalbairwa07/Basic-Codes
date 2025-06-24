01a96cfa7a06390d0e30161384d422e49a9db8f8
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-06-24 16:42:19 +0530

    Reverse_number

--- Reverse_number.cpp
+++ Reverse_number.cpp
@@ -12,3 +12,16 @@ int main(){
     
     return 0;
 }
+/* FOR LOOP VERSION
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter = ";
+    cin>>n;
+    for(int i=n; i>0; i=i/10){
+        int lastdig = i%10;
+        cout<<""<<lastdig;
+    }
+    return 0;
+}*/
